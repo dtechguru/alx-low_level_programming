dcf4d4cf33099273a832996708672e6e99573693
dtechguru <peculiarabioduns@gmail.com>
2023-02-19 19:49:30 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,14 @@
 /**
  * main - Prints the last digit of any randomly generated number
  *and whether it is greater than 5, less than 6, or 0.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
-	srand(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
