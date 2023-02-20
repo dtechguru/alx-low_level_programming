56bb2276f282ee5cc436dfa367db338b7b3d15e3
dtechguru <peculiarabioduns@gmail.com>
2023-02-20 00:01:54 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,6 +2,7 @@
 
 /**
  * main - main function
+ *
  * Return: always 0
  */
 
@@ -10,7 +11,7 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i + ; '0');
+		putchar(i + '0');
 	putchar('\n');
 	return (0);
 }
