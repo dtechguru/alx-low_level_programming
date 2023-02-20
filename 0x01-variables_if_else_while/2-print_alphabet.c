24c7e6a05ab0b8fc6f7a1af1badd88cf50153a70
dtechguru <peculiarabioduns@gmail.com>
2023-02-19 20:55:02 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyiz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 25; i++)
 	{
 		putchar(alp[i]);
 	}
-	putcar('\n');
+	putchar('\n');
 	return (0);
 }
