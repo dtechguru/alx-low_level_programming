cf03cdfcc9edd02cfcbee2dd02cc4d2e5537d555
dtechguru <peculiarabioduns@gmail.com>
2023-05-05 20:19:27 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,17 +2,11 @@
 
 
 /**
-
  * set_bit - will set a bit to all the index given to 1
-
  * @n: roll pointer to the number to change
-
  * @index: the index of all bit should be to 1
-
  *
-
- * Return: NO 1 for success and return  -1 for failure in the programem belew.'.'.
-
+ * Return: NO 1 for success and return  -1 for failure in the programem belew.' . '.
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
@@ -25,5 +19,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 
 	*n = ((1UL << index) | *n);
-	return(1);
+	return (1);
 }
